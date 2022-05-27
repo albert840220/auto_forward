29da6d07564ecd1b44efa7b6e7f7f8dee8624619
albert840220 <71442981+albert840220@users.noreply.github.com>
2022-05-27 09:32:03 +0800

    使用現成套件解碼郵件內容
    1. from email.parser import BytesParser
    2. 機敏資訊用變數儲存

--- server/src/runserver.py
+++ server/src/runserver.py
@@ -1,61 +1,88 @@
 import smtpd
 import asyncore
-import requests
-import json
-import logging
+import logging, sys, datetime
+import smtplib, ssl
+from email.policy import default
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+# parser package
+import quopri
+from io import BytesIO
+from email.parser import BytesParser
 
-logging.basicConfig(level=logging.INFO, filename='test.log',
-	format='[%(asctime)s %(levelname)-8s]\n %(message)s',
-	datefmt='%Y%m%d %H:%M:%S',
-	)
+
+logging.basicConfig(level=logging.INFO, filename='receive.log',
+                    format='[%(asctime)s %(levelname)-4s]\n %(message)s',
+                    datefmt='%Y%m%d %H:%M:%S',
+                    )
+ALLOWED_HOSTS = [""]  # ip list
+MAIL_RELAY_PORT = 25
+MAIL_SERVER = ""
+MAIL_PORT = 465
+SENDER_EMAIL = ""
+PASSWORD = ""
+
+
+def current_time():
+    data = datetime.datetime.utcnow()
+    return str(datetime.datetime.strftime(data, '%Y%m%d %H:%M:%S'))
+
 
 class CustomSMTPServer(smtpd.SMTPServer):
-    def process_message(self, peer, mailfrom, rcpttos, data, mail_options=None,rcpt_options=None):
-        print("function in")
-        ALLOWED_HOSTS = ["192.168.55.25","192.168.55.26","192.168.55.29"]
-        if peer[0] in ALLOWED_HOSTS:
-            str_data = str(data, 'utf-8') # raw_data is Bytes
-            data = str_data.split("\n")
-            print(data)
-            data_dict = {}
-            for i in range(0, len(data)):
-                if 'Subject:' in data[i]:
-                    list_split = data[i].split(": ")
-                    data_dict["subject"] = list_split[1]
-                    continue
-                if 'From:' in data[i]:
-                    list_split = data[i].split(": ")
-                    data_dict["sender"] = list_split[1]
-                    continue
-                if 'To:' in data[i]:
-                    list_split = data[i].split(": ")
-                    data_dict["recipients"] = [list_split[1]]
-                    continue
+    def process_message(self, peer, mailfrom, rcpttos, data, mail_options=None,
+                        rcpt_options=None):
+        print(f'{current_time()} function in')
+        headers = BytesParser(policy=default).parsebytes(data)
+        RECEIVER_EMAIL = str(headers['To']).split(", ")
+        html = headers.get_body(preferencelist='html')
+        plain = headers.get_body(preferencelist='plain')
+        
+        if html:
+            # decode quoted-printable in html
+            outputFile = BytesIO()
+            inputFile = BytesIO(bytes(html))  # create binary input file
+            quopri.decode(inputFile, outputFile)  # generate decoded data
+            output = outputFile.getvalue()  # extract output
+            html = output.decode('utf-8')  # extract text        
+        if plain:
+            for part in headers.walk():
+                payload = part.get_payload(decode=True)
+                if payload:
+                    plain = payload.decode("utf-8")
 
-            # spell word
-            text_list = data[7:]
-            text = ""
-            for word in text_list:
-                text += word + "\n"
-                
-            # Data processed as json
-            data_dict["body"] = text
-            
+        if peer[0] in ALLOWED_HOSTS:
+            # Create MIMEMultipart object
+            msg = MIMEMultipart()
+            msg["Subject"] = headers["subject"]
+            msg["From"] = SENDER_EMAIL
+            msg["To"] = ", ".join(RECEIVER_EMAIL)
+            if html:
+                msg.attach(MIMEText(html, "html"))
+            if plain:
+                msg.attach(MIMEText(plain, "plain"))
+
+            context = ssl.create_default_context()
             try:
-                h_data = {'Authorization': 'Basic ' + 'bm90aWZpY2F0aW9uOjM4MzZ5MWVkNzQ5ejQ3ejI4MGUyNXk2M2Y5N2I5OHlj'}
-                result = requests.post(
-                    "http://127.0.0.1:8002" + "/notification/send_mail/",
-                    json=data_dict, headers=h_data)
-                result_json = json.loads(result.content)
-                logging.info('IP:'+peer[0]+"\nMsg:"+str(result_json['stat_code'])+'\nFrom:'+data_dict["sender"]+'\nTo:'+str(data_dict["recipients"])+'\nSubject:'+data_dict["subject"]+'\nBody:'+data_dict["body"])
-                return
+                with smtplib.SMTP_SSL(MAIL_SERVER, MAIL_PORT,
+                                      context=context) as server:
+                    server.login(SENDER_EMAIL, PASSWORD)
+                    server.sendmail(
+                        SENDER_EMAIL, RECEIVER_EMAIL, msg.as_string()
+                    )
+                print(f'{current_time()} send mail success')
             except:
-                print("Send_mail API Error")
-        else:
-            print("invalid IP")
+                print(f'{current_time()} send mail fail')
+
+            try:
+                logging.info('IP:' + peer[0] + '\nFrom:' + headers["From"]
+                             + '\nTo:' + headers["To"] + '\nSubject:'
+                             + headers["Subject"] + "\n")
+            except:
+                print(f'{current_time()} LOGGING ERROR')
+            sys.stdout.flush()
+
 
 if __name__ == "__main__":
-    server = CustomSMTPServer(('0.0.0.0', 5025), None)
-    print('server in')
+    server = CustomSMTPServer(('0.0.0.0', MAIL_RELAY_PORT), None)
+    print(f'{current_time()} server in')
     asyncore.loop()
-
